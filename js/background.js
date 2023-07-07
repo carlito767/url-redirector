af96b5479c5c23d6c442a995be4919af1e486615
carlito767 <charles.longuemart@gmail.com>
2023-07-08 00:11:58 +0200

    improve regex for Twitter

--- js/background.js
+++ js/background.js
@@ -9,7 +9,12 @@ chrome.webRequest.onBeforeRequest.addListener(
 );
 
 function load() {
-  rules.push({ sourcePattern: "^https://twitter.com/([A-Za-z0-9_]+)[/]?$", destinationPattern: "https://tweettunnel.com/$1" });
+  // https://twitter.com/SCREEN_NAME
+  // https://twitter.com/SCREEN_NAME/
+  // https://twitter.com/SCREEN_NAME?parameters
+  rules.push({ sourcePattern: "^https://twitter.com/([A-Za-z0-9_]+)(/?|\\?.*)$", destinationPattern: "https://tweettunnel.com/$1" });
+  // https://twitter.com/SCREEN_NAME/status/TWEET_ID
+  // https://twitter.com/SCREEN_NAME/status/TWEET_ID?parameters
   rules.push({ sourcePattern: "^https://twitter.com/.+/status/([0-9]+).*$", destinationPattern: "https://platform.twitter.com/embed/Tweet.html?id=$1" });
 }
 
