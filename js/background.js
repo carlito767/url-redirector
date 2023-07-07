e46a4f454bdfd8222835cdcee4660316ba722430
carlito767 <charles.longuemart@gmail.com>
2023-07-07 15:32:41 +0200

    improve regex for Twitter username

--- js/background.js
+++ js/background.js
@@ -9,7 +9,7 @@ chrome.webRequest.onBeforeRequest.addListener(
 );
 
 function load() {
-  rules.push({ sourcePattern: "^https://twitter.com/([^/]+)$", destinationPattern: "https://tweettunnel.com/$1" });
+  rules.push({ sourcePattern: "^https://twitter.com/([A-Za-z0-9_]+)[/]?$", destinationPattern: "https://tweettunnel.com/$1" });
   rules.push({ sourcePattern: "^https://twitter.com/.+/status/([0-9]+).*$", destinationPattern: "https://platform.twitter.com/embed/Tweet.html?id=$1" });
 }
 
