f235a8231c2a71eced75e802e658acf282ce8098
carlito767 <charles.longuemart@gmail.com>
2023-10-11 16:30:49 +0200

    define rules per hostname

--- js/background.js
+++ js/background.js
@@ -1,33 +1,41 @@
-let rules = [];
+const rulesMap = {
+  "twitter.com": [
+    // https://twitter.com/SCREEN_NAME
+    // https://twitter.com/SCREEN_NAME/
+    // https://twitter.com/SCREEN_NAME?parameters
+    {
+      sourcePattern: "^https://twitter.com/([A-Za-z0-9_]+)(/?|\\?.*)$",
+      destinationPattern: "https://nitter.net/$1",
+    },
+    // https://twitter.com/SCREEN_NAME/status/TWEET_ID
+    // https://twitter.com/SCREEN_NAME/status/TWEET_ID?parameters
+    {
+      sourcePattern: "^https://twitter.com/(.+)/status/([0-9]+).*$",
+      destinationPattern: "https://nitter.net/$1/status/$2#m"
+    },
+  ],
+};
 
 chrome.webRequest.onBeforeRequest.addListener(
-  function (details) { return filterRequest(details); },
+  function (details) {
+    const url = new URL(details.url);
+    const hostname = url.hostname;
+
+    if (rulesMap.hasOwnProperty(hostname)) {
+      const rules = rulesMap[hostname];
+      for (let i = 0; i < rules.length; i++) {
+        let regex = new RegExp(rules[i].sourcePattern);
+        if (regex.test(details.url)) {
+          const url = details.url.replace(regex, rules[i].destinationPattern);
+          return { redirectUrl: url };
+        }
+      }
+
+      return { cancel: false };
+    }
+  },
   {
     urls: ["<all_urls>"],
   },
   ["blocking"]
 );
-
-function load() {
-  // https://twitter.com/SCREEN_NAME
-  // https://twitter.com/SCREEN_NAME/
-  // https://twitter.com/SCREEN_NAME?parameters
-  rules.push({ sourcePattern: "^https://twitter.com/([A-Za-z0-9_]+)(/?|\\?.*)$", destinationPattern: "https://nitter.net/$1" });
-  // https://twitter.com/SCREEN_NAME/status/TWEET_ID
-  // https://twitter.com/SCREEN_NAME/status/TWEET_ID?parameters
-  rules.push({ sourcePattern: "^https://twitter.com/(.+)/status/([0-9]+).*$", destinationPattern: "https://nitter.net/$1/status/$2#m" });
-}
-
-function filterRequest(details) {
-  for (let i = 0; i < rules.length; i++) {
-    let regex = new RegExp(rules[i].sourcePattern);
-    if (regex.test(details.url)) {
-      const url = details.url.replace(regex, rules[i].destinationPattern);
-      return { redirectUrl: url };
-    }
-  }
-
-  return { cancel: false };
-}
-
-document.addEventListener('DOMContentLoaded', load);
